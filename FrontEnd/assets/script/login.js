addccbf9566b265797760c9add32d5ca84cc3471
littlekanine <elia.kopff3891@gmail.com>
2023-12-18 18:44:07 +0100

    wip_12

--- FrontEnd/assets/script/login.js
+++ FrontEnd/assets/script/login.js
@@ -1,55 +1,100 @@
+// Variable
+
 const form = document.querySelector("form");
 const baliseEmail = document.getElementById("email");
+const balisePassword = document.getElementById("password")
 let regexMail = new RegExp ("[a-z0-9._-]+@[a-z0-9._-]+\.[a-z0-9._-]+");
-let regexPassword = new RegExp ("[a-z0-9._-]")
+let regexPassword = new RegExp ("[a-z0-9._-]+")
 
-Function
+// Function
 
-async function fetchData(apiEndPoint) {
+async function fetchToken() {
     try {
-        const response = await fetch(`http://localhost:5678/api/users/${apiEndPoint}`)
+        const response = await fetch (`http://localhost:5678/api/users/login`, {
+            method: `POST`,
+            headers : {"Content-Type":"application/json" }
+        })
             .then((response) => {
                 switch(response.status) {
-                    case 200:
-                        return response.json();
-                    case 500:
-                    default:
-                        throw new Error(`Échec de la requête avec le code d'état : ${response.status}`);
+                    case 200 : 
+                        return response.json
+                    default :
+                        throw new Error(`Échec de la requête avec le code d'état : ${response.status}`); 
                 }
             })
             .then((response) => {
-                return response;
+                                return response;
             });
-        
-        return response;
-    } catch (error) {
-          console.error(`Une erreur s'est produite : ${error.message}`);
+        return response
+    } catch(e) {
+        console.log("probleme")
+    }
+    console.log(response)
+}
+
+function checkEmail (baliseEmail) {
+    const valueEmail = baliseEmail.value;
+
+    if (regexMail.test(valueEmail)) {
+        baliseEmail.classList.add("valid");
+        baliseEmail.classList.remove("invalid");
+        console.log("correct")
+    } else {
+        baliseEmail.classList.add("invalid");
+        baliseEmail.classList.remove("valid");
+            throw new Error ("The email is incorrect ");
     }
 }
-async function fetchCategories() {
-    return fetchData("login")
-}
 
-// Email check 
-
-form.addEventListener("submit", (event) => {
-    event.preventDefault();
-
-    const baliseEmail = document.getElementById("email")
-    const valueEmail = baliseEmail.value
-    const balisePassword = document.getElementById("password")
+function checkPassword(balisePassword) {
     const valuePAssword = balisePassword.value
 
-        switch (valueEmail) {
-            case (regexMail.test(valueEmail)) :
-            baliseEmail.classList.add("valid")
-        }
+    if (regexPassword.test(valuePAssword)) {
+        balisePassword.classList.add("valid")
+        balisePassword.classList.remove("invalid")
+    } else {
+        balisePassword.classList.add("invalid")
+        balisePassword.classList.remove("valid")
+            throw new Error ("The password is incorrect ")
+    }
+}
 
-        // if (regexMail.test (valueEmail)) {
-        //     baliseEmail.classList.add("valid")
-        //     balisePassword.classList.add("valid")
-        // } else {
-        //     baliseEmail.classList.add("false")
-        //     balisePassword.classList.add("false")
-        // }
+form.addEventListener("submit", (event) => {
+        event.preventDefault();
+            try {
+                checkEmail (baliseEmail)
+                checkPassword(balisePassword)
+            } catch {
+
+            }
+        
 })
+
+
+// async function fetchToken() {
+//     try {
+//         const response = await fetch(`http://localhost:5678/api/users/login`)
+//             .then((response) => {
+//                 switch(response.status) {
+//                     case 200:
+//                         return response.json();
+//                     case 401 :
+//                         throw new Error(`Échec de la requête avec le code d'état : ${response.status}`);
+//                     default:
+//                         throw new Error(`Échec de la requête avec le code d'état : ${response.status}`);
+//                 }
+//             })
+//             .then((response) => {
+//                 return response;
+//             });
+        
+//         return response;
+    
+//     } catch (error) {
+//           console.error(`Une erreur s'est produite : ${error.message}`);
+//     }
+// }
+
+// async function fetchLogin() {
+//     return fetchToken("login");
+// }
