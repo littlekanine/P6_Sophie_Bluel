c2999ac2661b86e1aa818da737a732c76f33735c
littlekanine <elia.kopff3891@gmail.com>
2023-12-19 18:01:15 +0100

    wip_13

--- FrontEnd/assets/script/login.js
+++ FrontEnd/assets/script/login.js
@@ -1,100 +1,68 @@
 // Variable
-
-const form = document.querySelector("form");
+const formLogin = document.querySelector("form");
 const baliseEmail = document.getElementById("email");
-const balisePassword = document.getElementById("password")
-let regexMail = new RegExp ("[a-z0-9._-]+@[a-z0-9._-]+\.[a-z0-9._-]+");
-let regexPassword = new RegExp ("[a-z0-9._-]+")
+const balisePassword = document.getElementById("password");
+let apiToken = ""
+const inputContainer = document.getElementById("inputContainer")
+const errorMessage = document.createElement("p")
+errorMessage.classList.add("errorMessage")
+inputContainer.appendChild(errorMessage)
 
-// Function
+// Event Listener
 
-async function fetchToken() {
+formLogin.addEventListener("submit", async (event) => {
+    event.preventDefault();
+
+    const email = baliseEmail.value;
+    const password = balisePassword.value;
+
     try {
-        const response = await fetch (`http://localhost:5678/api/users/login`, {
-            method: `POST`,
-            headers : {"Content-Type":"application/json" }
-        })
-            .then((response) => {
-                switch(response.status) {
-                    case 200 : 
-                        return response.json
-                    default :
-                        throw new Error(`Échec de la requête avec le code d'état : ${response.status}`); 
-                }
-            })
-            .then((response) => {
-                                return response;
-            });
-        return response
-    } catch(e) {
-        console.log("probleme")
+        const token = await fetchDataLogin(email, password);
+
+        if (token) {
+            window.location.href = "/FrontEnd/index.html";
+        } else {
+            baliseEmail.classList.add("invalid")
+            balisePassword.classList.add("invalid")
+            errorMessage.textContent = "Input error: invalid email or password"
+            console.error("Token not recovered. Failed connection.");
+        }
+
+    } catch(error){
+        console.error("An error has occurred :", error);
     }
-    console.log(response)
-}
+});
 
-function checkEmail (baliseEmail) {
-    const valueEmail = baliseEmail.value;
 
-    if (regexMail.test(valueEmail)) {
-        baliseEmail.classList.add("valid");
-        baliseEmail.classList.remove("invalid");
-        console.log("correct")
-    } else {
-        baliseEmail.classList.add("invalid");
-        baliseEmail.classList.remove("valid");
-            throw new Error ("The email is incorrect ");
+// function for data recovery from the API
+
+async function fetchDataLogin(email, password) {
+    try {
+      const response = await fetch("http://localhost:5678/api/users/login", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ email, password }),
+
+        });
+
+        if (!response.ok) {
+        throw new Error(`Erreur HTTP! Statut : ${response.status}`);
+        }
+
+        const data = await response.json();
+
+        if ("token" in data && "userId" in data) {
+        apiUserId = data.userId;
+         apiToken = data.token;
+
+        console.log("Token retrieved from the API:", apiToken);
+        console.log("User ID retrieved from the API:", apiUserId);
+
+        } else {
+        console.error("API data is incomplete.");
+        }
+        return apiToken
+    } catch (error) {
+      console.error("Error retrieving data:", error);
     }
 }
-
-function checkPassword(balisePassword) {
-    const valuePAssword = balisePassword.value
-
-    if (regexPassword.test(valuePAssword)) {
-        balisePassword.classList.add("valid")
-        balisePassword.classList.remove("invalid")
-    } else {
-        balisePassword.classList.add("invalid")
-        balisePassword.classList.remove("valid")
-            throw new Error ("The password is incorrect ")
-    }
-}
-
-form.addEventListener("submit", (event) => {
-        event.preventDefault();
-            try {
-                checkEmail (baliseEmail)
-                checkPassword(balisePassword)
-            } catch {
-
-            }
-        
-})
-
-
-// async function fetchToken() {
-//     try {
-//         const response = await fetch(`http://localhost:5678/api/users/login`)
-//             .then((response) => {
-//                 switch(response.status) {
-//                     case 200:
-//                         return response.json();
-//                     case 401 :
-//                         throw new Error(`Échec de la requête avec le code d'état : ${response.status}`);
-//                     default:
-//                         throw new Error(`Échec de la requête avec le code d'état : ${response.status}`);
-//                 }
-//             })
-//             .then((response) => {
-//                 return response;
-//             });
-        
-//         return response;
-    
-//     } catch (error) {
-//           console.error(`Une erreur s'est produite : ${error.message}`);
-//     }
-// }
-
-// async function fetchLogin() {
-//     return fetchToken("login");
-// }
